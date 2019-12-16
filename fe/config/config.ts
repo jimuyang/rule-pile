1acf81e1dfaaf2661833856ffb97470d1dde7c48
杨帆 <fan.yang19@ele.me>
2019-12-16 17:01:16 +0800

    feat: fe 引入流程编辑器

--- fe/config/config.ts
+++ fe/config/config.ts
@@ -1,15 +1,13 @@
 import { IConfig, IPlugin } from 'umi-types';
 import defaultSettings from './defaultSettings'; // https://umijs.org/config/
+
 import slash from 'slash2';
 import themePluginConfig from './themePluginConfig';
-
-const { pwa } = defaultSettings;
-
-// preview.pro.ant.design only do not use in your production ;
+const { pwa } = defaultSettings; // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
+
 const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION } = process.env;
 const isAntDesignProPreview = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site';
-
 const plugins: IPlugin[] = [
   [
     'umi-plugin-react',
@@ -114,6 +112,12 @@ export default {
               authority: ['admin'],
             },
             {
+              name: '流程编辑器',
+              icon: 'smile',
+              path: '/editorflow',
+              component: './EditorFlow',
+            },
+            {
               component: './404',
             },
           ],
@@ -123,7 +127,6 @@ export default {
         },
       ],
     },
-
     {
       component: './404',
     },
@@ -174,8 +177,7 @@ export default {
   },
   manifest: {
     basePath: '/',
-  },
-  // chainWebpack: webpackPlugin,
+  }, // chainWebpack: webpackPlugin,
   // proxy: {
   //   '/server/api/': {
   //     target: 'https://preview.pro.ant.design/',
---
+++ fe/src/pages/EditorFlow/locales/en-US.ts
@@ -0,0 +1,4 @@
+export default {
+  'editorflow.description':
+    'The flow chart is an excellent way to represent the idea of the algorithm',
+};
