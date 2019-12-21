a74145f8cd6cc862a2284c7097ed8f56a40f69bd
杨帆 <fan.yang19@ele.me>
2019-12-22 00:58:17 +0800

    feat: restart fe

--- server/src/main/java/muyi/rule/pile/controller/RuleController.java
+++ server/src/main/java/muyi/rule/pile/controller/RuleController.java
@@ -1,9 +1,12 @@
 package muyi.rule.pile.controller;
 
+import muyi.rule.pile.dao.RuRuleDao;
 import muyi.rule.pile.dto.RuleDTO;
 import muyi.rule.pile.exception.RuleException;
+import muyi.rule.pile.po.RuRule;
 import muyi.rule.pile.service.RuleService;
-import org.apache.tomcat.util.digester.Rule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rule")
 public class RuleController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RuleController.class);
+
+    @Autowired
+    private RuRuleDao ruleDao;
 
     @Autowired
     private RuleService ruleService;
@@ -24,6 +31,16 @@ public class RuleController {
 
     @GetMapping("/test")
     public RuleDTO test() throws RuleException {
+        RuRule rule = ruleDao.selectRule(1L);
+        if (rule == null) {
+            rule = new RuRule();
+            rule.setCode("FirstCode");
+            rule.setName("第一个规则");
+            rule.setLimitScenes("");
+            ruleDao.insertRule(rule);
+        }
+        LOGGER.info("第一条规则: {}", rule);
+
         return this.ruleService.get(1L);
 //        throw new RuleException("test exception");
     }
